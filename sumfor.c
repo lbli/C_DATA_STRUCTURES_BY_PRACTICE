f411201c0460b639343eb972554e101c534a452c
lileibin <877455661@qq.com>
2018-10-03 21:09:48 +0800

    practice for for statements

---
+++ sumfor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    int n;
+    int num,sum = 0, avg = 0;
+
+    int count = 1;
+
+    printf( "Enter value of N:\n" );
+    scanf( "%d", &n );
+
+    //while ( count <= n )
+    //
+    for( count = 1; count <= n;count ++)
+    {
+        printf( "\n Enter value of %d number:", count );
+        scanf( "%d", &num );
+
+        sum += num;
+       // count ++;
+    }
+    //while ( count <= n);
+    avg = sum / n;
+
+    printf( "sum of %d numbers = %d\n",n,sum );
+    printf( "\nAverage of %d numbers = %d\n",n,avg );
+
+
+
+    return 0;
+}
